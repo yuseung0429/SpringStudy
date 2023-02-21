c5e4215539c1e661448d10553345d1fe148eb309
yuseung0429 <yuseung0429@naver.com>
2023-02-21 20:38:45 +0900

    Autowired and Qualifier

--- src/spring/di/ui/GridExamConsole.java
+++ src/spring/di/ui/GridExamConsole.java
@@ -1,6 +1,7 @@
 package spring.di.ui;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 import spring.di.entity.Exam;
 
@@ -21,6 +22,7 @@ public class GridExamConsole implements ExamConsole {
 		System.out.println("戌式式式式式式式式式式式式戎");
 	}
 	@Autowired
+	@Qualifier("exam2")
 	@Override
 	public void setExam(Exam exam) {
 		this.exam = exam;
